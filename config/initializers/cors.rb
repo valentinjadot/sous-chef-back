ba4f13d94d54749b038b8d6b04957f5fe2dcc8de
Valentin <valentinjadot@gmail.com>
2022-09-26 14:57:45 -0300

    chore(hosts) : improve CORS policy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://sous-chef.vercel.app'
+    if Rails.env.development?
+      origins 'localhost:3000', 'localhost:3001', 'https://sous-chef-staging.vercel.app/'
+    else
+      origins 'https://my-app.com'
+    end
+    origins 'https://sous-chef.vercel.app', 'https://sous-chef-staging.vercel.app/'
     resource '*', headers: :any, methods: [:get, :post, :patch]
   end
 end
