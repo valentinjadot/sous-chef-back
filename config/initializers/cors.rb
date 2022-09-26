6f0f304c2c9dcfb4f9925759acdc590b31724c37
Valentin <valentinjadot@gmail.com>
2022-09-26 15:08:18 -0300

    chore(hosts) : correct /

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     if Rails.env.development?
-      origins 'localhost:3000', 'localhost:3001', 'https://sous-chef-staging.vercel.app/'
+      origins 'localhost:3000', 'localhost:3001', 'https://sous-chef-staging.vercel.app'
     else
-      origins 'https://my-app.com'
+      origins 'https://sous-chef.vercel.app', 'https://sous-chef-staging.vercel.app'
     end
-    origins 'https://sous-chef.vercel.app', 'https://sous-chef-staging.vercel.app/'
     resource '*', headers: :any, methods: [:get, :post, :patch]
   end
 end
